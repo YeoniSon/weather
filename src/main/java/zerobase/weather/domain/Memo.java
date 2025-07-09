4eaefe5767f886207f934d8c25eb63a4f1add925
YeoniSon <sjy8211@gmail.com>
2025-07-10 00:18:34 +0900

    jpa로 데이터 저장하는 코드

--- src/main/java/zerobase/weather/domain/Memo.java
+++ src/main/java/zerobase/weather/domain/Memo.java
@@ -1,5 +1,9 @@
 package zerobase.weather.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +13,11 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity(name = "memo")
 public class Memo {
+    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY) // 가이드 값 mysql에 있는 값은 스프링이 만들지 않음
     private int id;
+
     private String text;
 }
