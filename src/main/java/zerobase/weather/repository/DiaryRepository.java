de3415bc30ae63b42bc0356dccb0ec01ceda1318
YeoniSon <sjy8211@gmail.com>
2025-07-11 00:25:49 +0900

    날씨 데이터 CRUD -> 조회, 수정, 삭제 controller, service, Repository 작성

--- src/main/java/zerobase/weather/repository/DiaryRepository.java
+++ src/main/java/zerobase/weather/repository/DiaryRepository.java
@@ -1,5 +1,6 @@
 package zerobase.weather.repository;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import zerobase.weather.domain.Diary;
@@ -11,5 +12,10 @@ import java.util.List;
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
     List<Diary> findAllByDate(LocalDate date);
 
-    List<Diary> findAllByDateBetween (LocalDate startDate, LocalDate endDate);
+    List<Diary> findAllByDateBetween(LocalDate startDate, LocalDate endDate);
+
+    Diary getFirstByDate(LocalDate date);
+
+    @Transactional
+    void deleteAllByDate(LocalDate date);
 }
