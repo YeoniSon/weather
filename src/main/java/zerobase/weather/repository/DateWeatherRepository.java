11ef70b8b31dd3a5d39d865529255772b11caa94
YeoniSon <sjy8211@gmail.com>
2025-07-11 23:19:13 +0900

    날씨 저장 스케쥴링 코드 작성

--- src/main/java/zerobase/weather/repository/DateWeatherRepository.java
+++ src/main/java/zerobase/weather/repository/DateWeatherRepository.java
@@ -1,4 +1,13 @@
 package zerobase.weather.repository;
 
-public class DateWeatherRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import zerobase.weather.domain.DateWeather;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface DateWeatherRepository extends JpaRepository<DateWeather, LocalDate> {
+    List<DateWeather> findByDate(LocalDate localDate);
 }
