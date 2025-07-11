20a20687b52d31db818757a5dabf9e6dbc255e1c
YeoniSon <sjy8211@gmail.com>
2025-07-11 15:56:09 +0900

    Spring 트랜잭션

--- src/main/java/zerobase/weather/WeatherApplication.java
+++ src/main/java/zerobase/weather/WeatherApplication.java
@@ -2,8 +2,10 @@ package zerobase.weather;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement //트랜젝션 활성화
 public class WeatherApplication {
 
     public static void main(String[] args) {
